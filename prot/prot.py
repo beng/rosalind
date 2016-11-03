75e2d17d00263739079d2af6342efb6d52e7a7e1
Benjamin Gelb <beg5670@gmail.com>
2016-11-02 20:02:58 -0400

    solution for prot

--- prot/prot.py
+++ prot/prot.py
@@ -13,11 +13,45 @@ Given: An RNA string s corresponding to a strand of mRNA (of length at most 10
 kbp).
 
 Return: The protein string encoded by s.
+
+Sample Dataset
+AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA
+
+Sample Output
+MAMAPRTEINSTRING
 """
 
 import re
-from itertools import permutations
+import sys
+from itertools import takewhile
+
+RNA_CODON_TBL = """
+UUU F      CUU L      AUU I      GUU V
+UUC F      CUC L      AUC I      GUC V
+UUA L      CUA L      AUA I      GUA V
+UUG L      CUG L      AUG M      GUG V
+UCU S      CCU P      ACU T      GCU A
+UCC S      CCC P      ACC T      GCC A
+UCA S      CCA P      ACA T      GCA A
+UCG S      CCG P      ACG T      GCG A
+UAU Y      CAU H      AAU N      GAU D
+UAC Y      CAC H      AAC N      GAC D
+UAA Stop   CAA Q      AAA K      GAA E
+UAG Stop   CAG Q      AAG K      GAG E
+UGU C      CGU R      AGU S      GGU G
+UGC C      CGC R      AGC S      GGC G
+UGA Stop   CGA R      AGA R      GGA G
+UGG W      CGG R      AGG R      GGG G
+"""
 
 
-def prot(s="AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"):
-    split = re.findall("...", s)
+def prot(seq):
+    codons = re.findall("...", seq)
+    rna_codons = RNA_CODON_TBL.split()
+    codon_map = dict(zip(rna_codons[::2], rna_codons[1::2]))
+    return takewhile(lambda v: v.lower() != 'stop', (map(codon_map.get, codons)))
+
+if __name__ == '__main__':
+    with open('rosalind_prot.txt') as fh:
+        print("".join(prot(fh.read())))
+    sys.exit(0)
